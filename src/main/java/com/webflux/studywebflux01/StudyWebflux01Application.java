c35adef16c8f769fa115e27be203c14f7c1604d6
chenpengw <chenpengw@foxmail.com>
2018-11-17 11:02:55 +0800

    初始化项目

---
+++ src/main/java/com/webflux/studywebflux01/StudyWebflux01Application.java
@@ -0,0 +1,12 @@
+package com.webflux.studywebflux01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyWebflux01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyWebflux01Application.class, args);
+    }
+}
